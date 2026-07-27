43e47a7ff382ccc49fefbe2330f7996242f4013c
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-07-27 18:24:26 +0100

    Enhance filter_pairs with price and threshold checks
    Updated the filter_pairs function to include price checks and minimum thresholds for liquidity and volume.

--- filters.py
+++ filters.py
@@ -1,16 +1,49 @@
-from config import MIN_LIQUIDITY, MIN_VOLUME
-
 def filter_pairs(pairs):
 
     good = []
 
+
     for pair in pairs:
 
-        liquidity = pair.get("liquidity", {}).get("usd", 0)
 
-        volume = pair.get("volume", {}).get("h24", 0)
+        liquidity = pair.get(
+            "liquidity",
+            {}
+        ).get(
+            "usd",
+            0
+        )
 
-        if liquidity >= MIN_LIQUIDITY and volume >= MIN_VOLUME:
-            good.append(pair)
+
+        volume = pair.get(
+            "volume",
+            {}
+        ).get(
+            "h24",
+            0
+        )
+
+
+        price = pair.get(
+            "priceUsd",
+            0
+        )
+
+
+        if not price:
+            continue
+
+
+        if liquidity < 10000:
+            continue
+
+
+        if volume < 20000:
+            continue
+
+
+        good.append(pair)
+
+
 
     return good
