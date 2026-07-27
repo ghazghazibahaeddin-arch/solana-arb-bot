443bbe90a706b8671bebe091a165de29656f4167
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-07-27 20:03:21 +0100

    Refactor config.py to load environment variables

--- config.py
+++ config.py
@@ -1,8 +1,58 @@
-# config.py
+import os
+from dotenv import load_dotenv
 
-DEX_API = "https://api.dexscreener.com/latest/dex/search?q=SOL"
 
-MIN_LIQUIDITY = 30000
-MIN_VOLUME = 100000
+load_dotenv()
 
-TOP_PAIRS = 30
+
+
+# APIs
+
+GEMINI_API_KEY = os.getenv(
+    "GEMINI_API_KEY"
+)
+
+
+GROQ_API_KEY = os.getenv(
+    "GROQ_API_KEY"
+)
+
+
+HELIUS_API_KEY = os.getenv(
+    "HELIUS_API_KEY"
+)
+
+
+BIRDEYE_API_KEY = os.getenv(
+    "BIRDEYE_API_KEY"
+)
+
+
+
+# Scanner
+
+SCAN_INTERVAL = 2
+
+
+
+# Trading Rules
+
+MIN_SCORE = 85
+
+
+MAX_DAILY_TRADES = 1000
+
+
+
+# Risk
+
+MIN_LIQUIDITY = 10000
+
+
+MAX_SLIPPAGE = 5
+
+
+
+# Network
+
+NETWORK = "mainnet-beta"
