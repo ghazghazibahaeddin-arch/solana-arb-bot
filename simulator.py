2e5f22a4a90b9ab5d71e7f1ca179b7413a53301d
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-07-27 18:30:01 +0100

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,13 +1,58 @@
-# simulator.py
+import random
+
 
 def simulate(pair):
 
-    liquidity = pair.get("liquidity", {}).get("usd", 0)
-    volume = pair.get("volume", {}).get("h24", 0)
+    liquidity = pair.get(
+        "liquidity",
+        {}
+    ).get(
+        "usd",
+        0
+    )
+
+
+    volume = pair.get(
+        "volume",
+        {}
+    ).get(
+        "h24",
+        0
+    )
+
 
     if liquidity == 0:
-        return 0
+        return {
+            "success":False,
+            "reason":"No liquidity"
+        }
 
-    ratio = volume / liquidity
 
-    return round(ratio, 2)
+    volume_ratio = volume / liquidity
+
+
+    slippage = random.uniform(
+        0.5,
+        5
+    )
+
+
+    expected_move = volume_ratio * 10
+
+
+    profit = expected_move - slippage
+
+
+
+    return {
+
+        "success":
+        profit > 5,
+
+        "expected_profit":
+        round(profit,2),
+
+        "slippage":
+        round(slippage,2)
+
+    }
