68ad7cc27ed6967e9e6431d163165b476d2e198e
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-07-28 09:30:19 +0100

    Refactor API key assignments in config.py
    Reorganized API key environment variable assignments and removed unused configuration settings.

--- config.py
+++ config.py
@@ -1,58 +1,16 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
-# APIs
-
-GEMINI_API_KEY = os.getenv(
-    "GEMINI_API_KEY"
+RPC_URL = os.getenv(
+    "RPC_URL",
+    "https://api.mainnet-beta.solana.com",
 )
 
-
-GROQ_API_KEY = os.getenv(
-    "GROQ_API_KEY"
-)
-
-
-HELIUS_API_KEY = os.getenv(
-    "HELIUS_API_KEY"
-)
-
-
-BIRDEYE_API_KEY = os.getenv(
-    "BIRDEYE_API_KEY"
-)
-
-
-
-# Scanner
-
-SCAN_INTERVAL = 2
-
-
-
-# Trading Rules
-
-MIN_SCORE = 85
-
-
-MAX_DAILY_TRADES = 1000
-
-
-
-# Risk
-
-MIN_LIQUIDITY = 10000
-
-
-MAX_SLIPPAGE = 5
-
-
-
-# Network
-
-NETWORK = "mainnet-beta"
+HELIUS_API_KEY = os.getenv("HELIUS_API_KEY", "")
+BIRDEYE_API_KEY = os.getenv("BIRDEYE_API_KEY", "")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+SOLSCAN_API_KEY = os.getenv("SOLSCAN_API_KEY", "")
